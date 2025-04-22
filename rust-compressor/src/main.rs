77c8d8601cb5c9e9d4653a6e889729d0ef0fb141
freshystar <thondipiongange@gmail.com>
2025-04-22 23:34:55 +0100

    feat: updated rust-compressor main file

--- rust-compressor/src/main.rs
+++ rust-compressor/src/main.rs
@@ -50,35 +50,3 @@ fn main() {
         _ => println!("Unsupported operation"),
     }
 }
-
-// fn main() -> std::io::Result<()> {
-//     let args: Vec<String> = env::args().collect();
-//     if args.len() != 4 {
-//         println!(
-//             "Usage: {} <input_file> <output_file> <compress/decompress>",
-//             args[0]
-//         );
-//         return Ok(());
-//     }
-
-//     let input_file = &args[1];
-//     let output_file = &args[2];
-//     let action = &args[3].to_lowercase();
-
-//     let window_size = 1024;
-//     match action.as_str() {
-//         "compress" => {
-//             let compressor = Compressor::new(window_size);
-//             compressor.compress_file(input_file, output_file)?;
-//         }
-//         "decompress" => {
-//             let decompressor = Decompressor::new(window_size);
-//             decompressor.decompress_file(input_file, output_file)?;
-//         }
-
-//         _ => {
-//             println!("Invalid action. Please use 'compress' or 'decompress' .");
-//         }
-//     }
-//     Ok(())
-// }
